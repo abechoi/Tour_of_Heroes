24a249d15083e74f2ebdbf9d1fb6e162f5791c59
abechoi <abraham.choi@icloud.com>
2020-01-25 23:05:11 -0800

    App Complete

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,6 +14,10 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes'; // URL to web api
 
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
+
   constructor(
     private messageService: MessageService,
     private http: HttpClient
@@ -29,13 +33,59 @@ export class HeroService {
   }
 
   getHero(id: number): Observable<Hero>{
+
     const url = `${this.heroesUrl}/${id}`;
+    
     return this.http.get<Hero>(url)
     .pipe(
       tap(_=> this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
     );
   }
+
+  updateHero(hero: Hero): Observable<any>{
+
+    return this.http.put(this.heroesUrl, hero, this.httpOptions)
+    .pipe(
+      tap(_=> this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero>{
+
+    return this.http.post<Hero>( this.heroesUrl, hero, this.httpOptions)
+    .pipe(
+      tap((newHero: Hero) => this.log(`added hero with id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero>{
+
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions)
+    .pipe(
+      tap(_=> this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+  
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]>{
+    if(!term.trim()){
+      // if not search term, return empty hero array
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`)
+    .pipe(
+      tap(_=> this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
 
   /*
     Handle Http operation that failed.
